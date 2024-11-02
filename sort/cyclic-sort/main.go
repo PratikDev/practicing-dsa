447e425bb04ce66025cedbb424958dcf2f1a1960
pratikdev <pratikdevofficial1@gmail.com>
2024-11-02 13:42:09 +0600

    cyclic sort

---
+++ sort/cyclic-sort/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{3, 4, 3, 5, 2}
+	cyclicSort(arr)
+	fmt.Println("Result:", arr)
+}
+
+func cyclicSort(arr []int) {
+	i := 0
+	for i < len(arr) {
+		value := arr[i]
+
+		if i+1 == value {
+			i++
+		} else {
+			// swap
+			arr[i] = arr[value-1]
+			arr[value-1] = value
+		}
+	}
+}
+
+func cyclicSortAlt(arr []int) {
+	last := len(arr) - 1
+	for i := 0; i <= last; i++ {
+		arr[i] = i + 1
+	}
+}
